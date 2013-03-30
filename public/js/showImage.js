4cb5d35d0651b185fad05bcddada2490f29d8c76
Derick Bailey <derickbailey@gmail.com>
2013-03-29 20:06:49 -0500

    converted ImageViewer to Backbone.View

--- public/js/showImage.js
+++ public/js/showImage.js
@@ -1,11 +1,29 @@
-ImageGallery.ImageViewer = {
-  init: function(){
-    this.$main = $("#main");
-    this.showImageTemplate = _.template($("#image-view-template").html());
-  },
+(function(){
 
-  show: function(image){
-    var html = this.showImageTemplate(image);
-    this.$main.html(html);
-  }
-};
+  ImageGallery.ImageViewer = {
+    init: function(){
+      this.$main = $("#main");
+    },
+
+    show: function(image){
+      var imageView = new ImageView({
+        model: image
+      });
+      this.$main.html(imageView.render().$el);
+    }
+  };
+
+  var ImageView = Backbone.View.extend({
+    
+    initialize: function(){
+      this.template = _.template($("#image-view-template").html());
+    },
+
+    render: function(){
+      var html = this.template(this.model);
+      this.$el.html(html);
+      return this;
+    }
+  });
+
+})(ImageGallery);
